53ed5b74032c9856ca5fc922297a57e0b302f695
Stoica Valentin <valentingstoica@yahoo.com>
2025-03-07 13:49:10 +0200

    .

--- solution/src/app/page.tsx
+++ solution/src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex h-[499px] w-[320px] flex-col gap-[24px] rounded-[10px] bg-white pt-[16px] pr-[16px] pb-[40px] pl-[16px]">
+    <main className="flex h-[499px] w-[320px] flex-col gap-[24px] rounded-[20px] bg-white pt-[16px] pr-[16px] pb-[40px] pl-[16px]">
       <Image src="/qr-code.png" alt="QR Code" width={288} height={288} />
       <section className="flex flex-col gap-[16px] pr-[16px] pl-[16px]">
         <article>
