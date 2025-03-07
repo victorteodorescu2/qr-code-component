7c3254dcdb56a556d25ffa4a2c18b97f4a7f0b28
Stoica Valentin <valentingstoica@yahoo.com>
2025-03-07 13:51:39 +0200

    Fix border rounded image

--- solution/src/app/page.tsx
+++ solution/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex h-[499px] w-[320px] flex-col gap-[24px] rounded-[20px] bg-white pt-[16px] pr-[16px] pb-[40px] pl-[16px]">
-      <Image src="/qr-code.png" alt="QR Code" width={288} height={288} />
+      <Image src="/qr-code.png" alt="QR Code" width={288} height={288} className="rounded-[10px]"/>
       <section className="flex flex-col gap-[16px] pr-[16px] pl-[16px]">
         <article>
           <p className="text-preset-1 text-slate-900 text-center">
